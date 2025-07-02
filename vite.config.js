8d541cd6aa5af7decb0de4f51bd68863df91f1d3
Sri Ganesh <T079@CIPL-ATC00239.colanonline.local>
2025-07-02 14:56:09 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // add this
-import tailwindcss from "tailwindcss"; // corrected import
-
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    // This ensures local dev also supports client-side routing
-    historyApiFallback: true,
-  },
+  plugins: [tailwindcss()],
+  base: "/", // for Vercel, keep this as '/'
 });
